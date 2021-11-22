80645233572c2b58d8e5af06062f730bccb00910
Rikardian <92721408+Rikardian@users.noreply.github.com>
2021-11-22 13:33:42 +0400

    Start Project.

---
+++ main/java/ru/ibs/happynes/Group7FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.ibs.happynes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group7FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group7FinalProjectApplication.class, args);
+    }
+
+}
